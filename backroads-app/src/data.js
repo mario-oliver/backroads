fdad26a7acbc06ba186b288947ebb656b7b01485
Mario Oliver <mariooliver@Marios-MacBook-Pro.local>
2023-05-19 15:52:36 -0400

    refactoring for react component-based dynamic html for footer, services and tours

--- backroads-app/src/data.js
+++ backroads-app/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -8,4 +13,68 @@ export const socialLinks = [
   { id: 1, href: 'https://www.twitter.com', icon: 'fab fa-facebook' },
   { id: 2, href: 'https://www.twitter.com', icon: 'fab fa-twitter' },
   { id: 3, href: 'https://www.twitter.com', icon: 'fab fa-squarespace' },
+]
+
+export const services = [
+  {
+    id: 2,
+    title: 'saving money',
+    icon: 'fas fa-wallet fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    title: 'endless hiking',
+    icon: 'fas fa-tree fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 1,
+    title: 'amazing comfort',
+    icon: 'fas fa-socks fa-fw',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    title: 'Tibet Adventure',
+    date: 'august 26th, 2020',
+    location: 'china',
+    duration: 6,
+    price: 2100,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    img: tour2,
+    title: 'Best of Java',
+    date: 'october 1st, 2020',
+    location: 'Indonesia',
+    duration: 11,
+    price: 1400,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    img: tour3,
+    title: 'Explore Hong Kong',
+    date: 'september 15th, 2020',
+    location: 'Hong Kong',
+    duration: 8,
+    price: 1700,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 4,
+    img: tour4,
+    title: 'Kenya Highlight',
+    date: 'august 10th, 2020',
+    location: 'Kenya',
+    duration: 20,
+    price: 3300,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
 ]
